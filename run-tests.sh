4809ebeea541a73941bf569ee62b3e24f6a5e2e1
Tom Hlava <hlava@cesnet.cz>
2020-08-26 15:00:18 +0200

    cleanup

--- run-tests.sh
+++ run-tests.sh
@@ -8,5 +8,5 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 # isort -rc -c -df **/*.py && \       # metapackage, isort not needed
-check-manifest --ignore ".travis-*" --ignore "invenio-integration-tests/*" && \
-python setup.py test
+check-manifest --ignore ".travis-*" && \
+pytest
