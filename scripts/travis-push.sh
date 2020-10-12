dfabf224a68ca2f5e41ac81ffb4e56b83390ad7c
Tom Hlava <hlava@cesnet.cz>
2020-10-12 19:01:49 +0200

    travis_push test1

---
+++ scripts/travis-push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "travis-push.sh"
+
+[[ "$1" == "dryrun" ]] && { echo "dryrun"; exit 0; }
+
+#BRANCH=invenio-3.3
+BRANCH=test-travis-push
+DATE=$(date '+%y%m%d-%H%M%S')
+
+git config --global user.name oarepo-bot
+git config --global user.email noreply@cesnet.cz
+
+git clone -q -b https://oarepo-bot:${OAR_BOT}@github.com/oarepo/oarepo-micro-api.git -b "$BRANCH" \
+ && date '+%y%m%d-%H%M%S' >> tests/.trig.txt \
+ && git add tests/.trig.txt \
+ && git commit -m "travis commit $DATE (build:$TRAVIS_BUILD_NUMBER result:$TRAVIS_TEST_RESULT)" -m "[skip ci]" \
+ && git push
