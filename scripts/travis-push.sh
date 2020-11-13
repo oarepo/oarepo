deec4e9feab11e6882380c45aa1da5093dbc2059
Tom Hlava <hlava@cesnet.cz>
2020-11-13 16:58:05 +0100

    changed push+tag new version number to oarepo-micro-api

--- scripts/travis-push.sh
+++ scripts/travis-push.sh
@@ -15,27 +15,27 @@ echo "travis-push.sh"
 DATE=$(date '+%y%m%d-%H%M%S')
 VERSION_PY='oarepo/version.py'
 # grab full version string:
-NEWVER=$(sed -n '/^__version__ / {s/^[^"]\+"\([0-9\.]\+\)"$/\1/;p}' "$VERSION_PY")
+OAREPO_VER=$(sed -n '/^__version__ / {s/^[^"]\+"\([0-9\.]\+\)"$/\1/;p}' "$VERSION_PY")
 # grab 2-number version string:
-NEWVER2=$(sed -n '/^[0-9\.]\+$/ {s/^\([0-9]\+\.[0-9]\+\)\..*$/\1/;p }' <<<"$NEWVER")
-BRANCH="invenio-$NEWVER2"
+OAREPO_VER2=$(sed -n '/^[0-9\.]\+$/ {s/^\([0-9]\+\.[0-9]\+\)\..*$/\1/;p }' <<<"$OAREPO_VER")
+BRANCH="invenio-$OAREPO_VER2"
 URL="https://oarepo-bot:${OAR_BOT}@github.com/oarepo/oarepo-micro-api.git"
 DIR=oarepo-micro-api
 
 git config --global user.name oarepo-bot
 git config --global user.email noreply@cesnet.cz
 
-{
-  git clone -q -b "$BRANCH" "$URL" "$DIR" \
-    || git clone -q -b master "$URL" "$DIR"
-} \
-  && ( cd "$DIR"; git checkout -B "$BRANCH"; ) \
-  && ./scripts/generate_micro-api_setup.sh "$NEWVER2" \
-  && cd "$DIR" \
-  && git add .travis.yml setup.py oarepo_micro_api/version.py \
-  && git commit -m "travis commit $DATE (build:$TRAVIS_BUILD_NUMBER result:$TRAVIS_TEST_RESULT)" \
-  && git push --set-upstream origin "$BRANCH" \
-  && git tag -a "$NEWVER" -m "based on oarepo $NEWVER" \
-  && git push origin "$NEWVER"
+# chechout and push+tag new version number to oarepo-micro-api
+git clone -q -b "$BRANCH" "$URL" "$DIR" || git clone -q -b master "$URL" "$DIR"
+( cd "$DIR"; git checkout -B "$BRANCH"; )
+./scripts/generate_micro-api_setup.sh "$OAREPO_VER2"
+cd "$DIR"
+MICROAPI_VERSION_PY='oarepo_micro_api/version.py'
+MICROAPI_VER=$(sed -n '/^__version__ / {s/^[^"\x27]\+["\x27]\([0-9.]\+\)["\x27]$/\1/;p}' "$MICROAPI_VERSION_PY")
+git add .travis.yml setup.py oarepo_micro_api/version.py
+git commit -m "travis commit $DATE (build:$TRAVIS_BUILD_NUMBER result:$TRAVIS_TEST_RESULT)"
+git push --set-upstream origin "$BRANCH"
+git tag -a "$MICROAPI_VER" -m "based on oarepo $MICROAPI_VER"
+git push origin "$MICROAPI_VER"
 
 echo "Done: $?"
