65324ec2804b19d55d8f221894e623d21975ecc9
Tom Hlava <hlava@cesnet.cz>
2022-12-05 17:13:42 +0100

    rdm-11 test3, opensearch2

--- run-tests.sh
+++ run-tests.sh
@@ -17,12 +17,9 @@ function cleanup(){
 trap cleanup EXIT
 
 ENV0="$(printenv)"
-
 echo "docker-services-cli up (DB:$DB; SEARCH:$SEARCH)"
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch7} --mq ${MQ:-redis} --env)"
-
 ENV1="$(printenv)"
-
 echo "env diff:"
 diff <(echo "$ENV0") <(echo "$ENV1") || true
 echo ""
