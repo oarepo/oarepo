bb3cb99a857e32400487d68c842e0faf7612cdcc
Mirek Simek <miroslav.simek@gmail.com>
2025-02-24 11:58:59 +0100

    Added ui parameters

--- oarepo/config/generic_parameters.py
+++ oarepo/config/generic_parameters.py
@@ -215,6 +215,9 @@ def configure_generic_parameters(
 
     WEBPACKEXT_PROJECT = "oarepo_ui.webpack:project"
 
+    DASHBOARD_RECORD_CREATE_URL = None
+    RECORD_ROUTES = None
+
     # RDM
     INVENIO_RDM_ENABLED = True
     RDM_PERSISTENT_IDENTIFIERS: dict = {}
