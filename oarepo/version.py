e61f35acb429545ce249e5c7cdf43f37d7b44b95
mesemus <3385642+mesemus@users.noreply.github.com>
2025-05-07 13:04:12 +0000

    [invenio-integration-tests] update requirements inside pyproject.toml to make release 12.2.12

--- oarepo/version.py
+++ oarepo/version.py
@@ -1,4 +1,4 @@
 
 # the first 3 numbers of version here must be the same as invenio version in setup.py
 # if the version ends with a*, add this to the first 3 numbers
-__version__ = "12.0.30"
+__version__ = "12.2.12"
