5b6a590d1a1a848049c5d16965cd4f45d822eea6
Miroslav Bauer <mirekys@gmail.com>
2025-09-29 17:25:44 +0200

    fix: function eval

--- tools/library_runner.sh
+++ tools/library_runner.sh
@@ -104,7 +104,7 @@ run_tools() {
     set -o pipefail
 
     # Parse the commandline according to the options defined above.
-    export OAREPO_VERSION=${OAREPO_VERSION:-${first_oarepo_version}}
+    export OAREPO_VERSION=${OAREPO_VERSION:-$(first_oarepo_version)}
     export PYTHON_VERSION=${PYTHON_VERSION:-"3.13"}
     export PYTHON=${PYTHON:-"python${PYTHON_VERSION}"}
     export NO_EDITABLE=${NO_EDITABLE:-""}
