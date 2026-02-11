a5653cd91471656386e8d793a9d0d1d800c2aa24
Miroslav Simek <3385642+mesemus@users.noreply.github.com>
2026-02-11 21:45:30 +0100

    Moved imports into the respective functions

--- oarepo/config/einfra.py
+++ oarepo/config/einfra.py
@@ -8,22 +8,32 @@
 #
 """Configuration for oarepo-oidc-einfra module."""
 import os
-from oarepo_oidc_einfra import EINFRA_LOGIN_APP
-from oarepo_oidc_einfra import config as oarepo_einfra_config
+import sys
 
-from .base import set_constants_in_caller, load_configuration_variables
+from .base import load_configuration_variables, set_constants_in_caller
 
 
 def configure_einfra_oidc() -> None:
+    try:
+        from oarepo_oidc_einfra import EINFRA_LOGIN_APP
+        from oarepo_oidc_einfra import config as oarepo_einfra_config
+    except ImportError:
+        print("Please install oarepo-oidc-einfra to use E-INFRA OIDC login.")
+        sys.exit(1)
+
     env = load_configuration_variables()
-    if os.environ.get("INVENIO_REMOTE_AUTH_ENABLED", "no").lower() in ("true", "yes", "1"):
+    if os.environ.get("INVENIO_REMOTE_AUTH_ENABLED", "no").lower() in (
+        "true",
+        "yes",
+        "1",
+    ):
         OAUTHCLIENT_REMOTE_APPS = {"e-infra": EINFRA_LOGIN_APP}
         # needed for disconnect
         EINFRA = dict(
             consumer_key=env.INVENIO_EINFRA_CONSUMER_KEY,
             consumer_secret=env.INVENIO_EINFRA_CONSUMER_SECRET,
             **{
-                k[len("EINFRA_"):].lower(): getattr(oarepo_einfra_config, k)
+                k[len("EINFRA_") :].lower(): getattr(oarepo_einfra_config, k)
                 for k in dir(oarepo_einfra_config)
                 if k.startswith("EINFRA_")
             },
--- oarepo/config/jobs.py
+++ oarepo/config/jobs.py
@@ -1,16 +1,18 @@
 from oarepo.config.base import set_constants_in_caller
 
-from oarepo_runtime.services.generators import AdministrationWithQueryFilter
-
-from invenio_jobs.services.permissions import (
-    JobLogsPermissionPolicy as InvenioJobLogsPermissionPolicy,
-)
-class JobLogsPermissionPolicy(InvenioJobLogsPermissionPolicy):
-    """Permission policy for job logs."""
-
-    can_read = [AdministrationWithQueryFilter()]
 
 def configure_jobs(permission_policy=None, logging_level=None):
+
+    from invenio_jobs.services.permissions import (
+        JobLogsPermissionPolicy as InvenioJobLogsPermissionPolicy,
+    )
+    from oarepo_runtime.services.generators import AdministrationWithQueryFilter
+
+    class JobLogsPermissionPolicy(InvenioJobLogsPermissionPolicy):
+        """Permission policy for job logs."""
+
+        can_read = [AdministrationWithQueryFilter()]
+
     # invenio-jobs configuration
     APP_LOGS_PERMISSION_POLICY = permission_policy or JobLogsPermissionPolicy
     JOBS_LOGGING_LEVEL = logging_level or "INFO"
