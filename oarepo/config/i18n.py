ad2fe4f7e5bc9c72b6da238cc6cf43082a08e6b3
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-03-23 11:59:24 +0100

    fix: moved imports inside of the function to prevent crashing in context of libraries

--- oarepo/config/i18n.py
+++ oarepo/config/i18n.py
@@ -1,6 +1,5 @@
-from flask_babel import lazy_gettext as _
-from marshmallow_i18n_messages import add_i18n_to_marshmallow
-
-
 def initialize_i18n():
+    from flask_babel import lazy_gettext as _
+    from marshmallow_i18n_messages import add_i18n_to_marshmallow
+
     add_i18n_to_marshmallow(_)
