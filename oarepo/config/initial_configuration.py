834a7b4c6ad7e0dbbdeb8b9b33a52337398e1d14
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2026-07-09 17:29:59 +0200

    removed configuration (#263)

--- oarepo/config/initial_configuration.py
+++
@@ -1,9 +0,0 @@
-"""This module provides initial configuration for OARepo-based repositories.
-
-It is registered as invenio_config.module entrypoint so is loaded early in
-the application initialization. This allows other modules to access/modify its
-configuration options.
-"""
-
-THEME_FRONTPAGE = False
-"""Enable frontpage theme."""
